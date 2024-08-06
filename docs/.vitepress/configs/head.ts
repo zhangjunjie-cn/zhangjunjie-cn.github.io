e59e9d8387077385ecfb182e208957f8a5ee83ce
zhangjunjie-cn <1324638439@qq.com>
2024-08-06 02:55:43 +0000

    回档平调

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -2,7 +2,7 @@ import type { HeadConfig } from "vitepress";
 
 export const head: HeadConfig[] = [
   // 像小鱼一样的鼠标
-  ['link', { rel: 'stylesheet', href: "/mouse/nomouse.scss" }],
+  ['link', { rel: 'stylesheet', href: "/mouse/nomouse.css" }],
   // 引入外部 JavaScript 文件
   ['script', { src: "/mouse/pointer.js" }],
 
