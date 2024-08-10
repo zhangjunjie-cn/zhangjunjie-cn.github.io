74f21202b3045b3f16a602d149e8d284ac9e7548
zhangjunjie-cn <118242553+zhangjunjie-cn@users.noreply.github.com>
2024-08-10 19:57:53 +0800

    Deploying to gh-pages from @ zhangjunjie-cn/zhangjunjie-cn.github.io@6fcaf97d82888b0b82def58f8137e5744c9b6239 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/APP_BASE_PATH/sw.js', { scope: '/APP_BASE_PATH/' })})}
