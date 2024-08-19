6270a1e418a5e52fbf6861937c0395ff6cd4ac54
zhangjunjie <118242553+zhangjunjie-cn@users.noreply.github.com>
2024-08-19 23:47:02 +0800

    Update home.vue

--- docs/.vitepress/theme/views/home.vue
+++ docs/.vitepress/theme/views/home.vue
@@ -3,7 +3,7 @@
 		<div class="video-container" v-cloak>
 			<video v-if="!curDeviceIsMobile" class="logo-video" autoplay preload="auto" loop muted>
 				<source
-					src="http://images-zjj.test.upcdn.net/template.mp4"
+					src="https://zhangjunjie.pages.dev/template.mp4"
 					type="video/mp4"
 				/>
 			</video>
