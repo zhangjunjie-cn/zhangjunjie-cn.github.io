2c6b6d6f3e33de118a0a1157163c50d8a00905d7
zhangjunjie-cn <1324638439@qq.com>
2025-03-23 09:35:45 +0000

    修改pwa读取方式【存在回档的可能】

--- docs/.vitepress/config/pwa.ts
+++ docs/.vitepress/config/pwa.ts
@@ -5,86 +5,86 @@ import type { VitePWAOptions } from "vite-plugin-pwa";
 const pwa: Partial<VitePWAOptions> = {
   // 根目录
   outDir: resolve(__dirname, "../../../dist"),
-  registerType: "autoUpdate",
-  // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-  includeManifestIcons: false,
-  manifest: {
-    id: "/",
-    name: "张俊杰的博客",
-    short_name: "张俊杰的博客",
-    description: "张俊杰的博客人生",
-    theme_color: '#ffffff',
-    icons: [
-      {
-        src: "/images/pwa-120x120.png",
-        sizes: "120x120",
-        type: "image/png",
-      },
-      {
-        src: "/images/pwa-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/images/pwa-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any",
-      },
-    ],
-  },
-  workbox: {
-    globPatterns: ["**/*.{css,js,html,svg,png,ico,txt,woff2}"],
-    maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
-    runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "google-fonts-cache",
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "gstatic-fonts-cache",
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "jsdelivr-images-cache",
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 60 * 60 * 24 * 7, // <== 7 days
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
-  },
-  // 请求页面不存在，强制返回404
-  strategies: 'generateSW', 
-  experimental: {
-    includeAllowlist: true
-  }
+  // registerType: "autoUpdate",
+  // // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+  // includeManifestIcons: false,
+  // manifest: {
+  //   id: "/",
+  //   name: "张俊杰的博客",
+  //   short_name: "张俊杰的博客",
+  //   description: "张俊杰的博客人生",
+  //   theme_color: '#ffffff',
+  //   icons: [
+  //     {
+  //       src: "/images/pwa-120x120.png",
+  //       sizes: "120x120",
+  //       type: "image/png",
+  //     },
+  //     {
+  //       src: "/images/pwa-192x192.png",
+  //       sizes: "192x192",
+  //       type: "image/png",
+  //     },
+  //     {
+  //       src: "/images/pwa-512x512.png",
+  //       sizes: "512x512",
+  //       type: "image/png",
+  //       purpose: "any",
+  //     },
+  //   ],
+  // },
+  // workbox: {
+  //   globPatterns: ["**/*.{css,js,html,svg,png,ico,txt,woff2}"],
+  //   maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
+  //   runtimeCaching: [
+  //     {
+  //       urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+  //       handler: "CacheFirst",
+  //       options: {
+  //         cacheName: "google-fonts-cache",
+  //         expiration: {
+  //           maxEntries: 10,
+  //           maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+  //         },
+  //         cacheableResponse: {
+  //           statuses: [0, 200],
+  //         },
+  //       },
+  //     },
+  //     {
+  //       urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+  //       handler: "CacheFirst",
+  //       options: {
+  //         cacheName: "gstatic-fonts-cache",
+  //         expiration: {
+  //           maxEntries: 10,
+  //           maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+  //         },
+  //         cacheableResponse: {
+  //           statuses: [0, 200],
+  //         },
+  //       },
+  //     },
+  //     {
+  //       urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
+  //       handler: "NetworkFirst",
+  //       options: {
+  //         cacheName: "jsdelivr-images-cache",
+  //         expiration: {
+  //           maxEntries: 10,
+  //           maxAgeSeconds: 60 * 60 * 24 * 7, // <== 7 days
+  //         },
+  //         cacheableResponse: {
+  //           statuses: [0, 200],
+  //         },
+  //       },
+  //     },
+  //   ],
+  // },
+  // // 请求页面不存在，强制返回404
+  // strategies: 'generateSW', 
+  // experimental: {
+  //   includeAllowlist: true
+  // }
 };
 export default pwa;
--- docs/dev-dist/sw.js
+++ docs/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cvg8hm1tbr"
+    "revision": "0.csr6jpk2qro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
